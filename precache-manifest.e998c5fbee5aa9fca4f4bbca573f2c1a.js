2c1db60427f649e4664f28a6577ecf8fc2610e32
Yash Panchal <panchaly75@gmail.com>
2022-07-27 16:36:33 +0530

    Updates

---
+++ precache-manifest.e998c5fbee5aa9fca4f4bbca573f2c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1dcc86dc61b60357e25e61c03104e0",
+    "url": "/RAiderV1.2/index.html"
+  },
+  {
+    "revision": "006ee9a931fda38acdcf",
+    "url": "/RAiderV1.2/static/css/main.2e48c50c.chunk.css"
+  },
+  {
+    "revision": "7a9b107ed89ab9e824c5",
+    "url": "/RAiderV1.2/static/js/2.15829a75.chunk.js"
+  },
+  {
+    "revision": "a2334ff9bc68c321bce67af5ebb4ec33",
+    "url": "/RAiderV1.2/static/js/2.15829a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006ee9a931fda38acdcf",
+    "url": "/RAiderV1.2/static/js/main.1956dd11.chunk.js"
+  },
+  {
+    "revision": "36211d089cd0752b02f6",
+    "url": "/RAiderV1.2/static/js/runtime-main.b1c6aa29.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RAiderV1.1/precache-manifest.4d54fdda1938ea7865651aaec5aa7a4b.js"
+  "/RAiderV1.2/precache-manifest.e998c5fbee5aa9fca4f4bbca573f2c1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RAiderV1.1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RAiderV1.2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
